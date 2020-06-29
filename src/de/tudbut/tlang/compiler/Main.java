1814cf0fd84fb1d2b87825c545852e0cafd7484a
Daniel H <dh.danielhister@gmail.com>
2020-06-29 21:32:50 +0200

    V1.1.0
    Everything bug-free and usable!

--- src/de/tudbut/tlang/compiler/Main.java
+++ src/de/tudbut/tlang/compiler/Main.java
@@ -2,33 +2,62 @@ package de.tudbut.tlang.compiler;
 
 import de.tudbut.tools.FileRW;
 import de.tudbut.tools.Tools;
+import de.tudbut.tools.bintools.BinFileRW;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Base64;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         System.out.print("\r\u001b[K");
-        String theString = new FileRW(args[0]).getContent().join("");
+        StringBuilder s = new StringBuilder();
+        if(args[0].endsWith(".tlg.b64")) {
+            BinFileRW binFile = new BinFileRW(args[0]);
+            byte[] toDecode = new byte[binFile.getBinContent().length];
+            for (int i = 0; i < toDecode.length; i++) {
+                toDecode[i] = (byte) binFile.getBinContent()[i];
+            }
+            for (byte b : Base64.getDecoder().decode(toDecode)) {
+                s.append((char) b);
+            }
+        }
+        else if(args[0].endsWith(".tlg")) {
+            s = new StringBuilder(new FileRW(args[0]).getContent().join(""));
+
+            byte[] toEncode = new byte[s.length()];
+            for (int i = 0; i < toEncode.length; i++) {
+                toEncode[i] = (byte) s.toString().toCharArray()[i];
+            }
+
+            toEncode = Base64.getEncoder().encode(toEncode);
+
+            int[] toWrite = new int[toEncode.length];
+            for (int i = 0; i < toWrite.length; i++) {
+                toWrite[i] = toEncode[i];
+            }
+            new BinFileRW(args[0] + ".b64").setBinContent(toWrite);
+
+            new FileRW("short_" + args[0]).setContent(s.toString().replaceAll("\n", ""));
+        }
+        String theString = s.toString();
         theString = theString
-                .replaceAll("%", "%P")
-                .replaceAll("\\\\\\*", "%T")
-                .replaceAll("\\\\-", "%S")
-                .replaceAll("\\\\#", "%B")
-                .replaceAll("\\\\<", "%L")
-                .replaceAll("\\\\>", "%R")
-                .replaceAll("\\\\'", "%U")
-                .replaceAll("\\\\,", "%D")
-                .replaceAll("\\\\\\(", "%1")
-                .replaceAll("\\\\\\)", "%2")
+                .replaceAll("\\\\\\*", "\u0000")
+                .replaceAll("\\\\-", "\u0001")
+                .replaceAll("\\\\#", "\u0002")
+                .replaceAll("\\\\<", "\u0003")
+                .replaceAll("\\\\>", "\u0004")
+                .replaceAll("\\\\'", "\u0005")
+                .replaceAll("\\\\,", "\u0006")
+                .replaceAll("\\\\\\(", "\u0007")
+                .replaceAll("\\\\\\)", "\u0008")
                 .replaceAll("\\\\n", "\n");
 
         boolean isInString = false;
         boolean wasInString = false;
         StringBuilder thatString = new StringBuilder();
         for (int i = 0; i < theString.length(); i++) {
-            Thread.sleep(1);
             if (theString.substring(i).startsWith("(")) {
                 isInString = true;
                 i++;
@@ -77,16 +106,15 @@ public class Main {
                 .replaceAll(",", "\u001b[B")
                 .replaceAll("-", "\u001b[K")
                 .replaceAll("#", "\r")
-                .replaceAll("%T", "*")
-                .replaceAll("%S", "-")
-                .replaceAll("%B", "#")
-                .replaceAll("%L", "<")
-                .replaceAll("%R", ">")
-                .replaceAll("%U", "'")
-                .replaceAll("%D", ",")
-                .replaceAll("%1", "(")
-                .replaceAll("%2", ")")
-                .replaceAll("%P", "%"));
+                .replaceAll("\\x{0000}", "*")
+                .replaceAll("\\x{0001}", "-")
+                .replaceAll("\\x{0002}", "#")
+                .replaceAll("\\x{0003}", "<")
+                .replaceAll("\\x{0004}", ">")
+                .replaceAll("\\x{0005}", "'")
+                .replaceAll("\\x{0006}", ",")
+                .replaceAll("\\x{0007}", "(")
+                .replaceAll("\\x{0008}", ")"));
         System.out.println();
     }
 }
